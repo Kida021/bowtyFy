f4e6a56443d98f8beda949f85d7245c7dba21d43
Marrah <30308273+Kida021@users.noreply.github.com>
2017-07-20 13:02:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ server.listen(process.env.port || process.env.PORT || 8080, function () {
 });
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "Your App ID Here",
-    appPassword: "Your App Password Here"
+    appId: "BOWTTY",
+    appPassword: "8a542b90-96c6-42ce-91ba-3aeb28470e62"
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
