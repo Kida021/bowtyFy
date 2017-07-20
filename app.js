79241e780b37de966c600fe193f9e2a4a4dae515
Marrah <30308273+Kida021@users.noreply.github.com>
2017-07-20 15:23:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ Wag kang, maniwala, kakastahin ka lang nila.\n
 Wag kang, paumaga, kakantutin lang nila.\n
 Wag mong paubaya, kakamkamin ka lang nila.\n
 Kakantutin ka lang nila`);
-      }eelse if(session.message.text.toLowerCase().contains('sing') | session.message.text.toLowerCase().contains('sings')){
+      }else if(session.message.text.toLowerCase().contains('sing') | session.message.text.toLowerCase().contains('sings')){
          session.send(`Here's the list of songs that I can sing!/n ambing /n momay..!`);
-      }lse{
+      }else{
         session.send(`Sorry I don't understand alien language please learn how to speak in english!! and talk to me again.....`);
       }
 });
