8b98e8910652c8e6191c66714e8cba9338cb2e89
Marrah <30308273+Kida021@users.noreply.github.com>
2017-07-21 15:23:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ var connector = new builder.ChatConnector({
 });
 // This is a dinner reservation bot that uses a waterfall technique to prompt users for input.
 var bot = new builder.UniversalBot(connector, [
-    function (session) {       
-        var name = session.user ? session.user.name : null;
+    function (session,message) {       
+        var name = message.user ? message.user.name : null;
         var reply = new builder.Message()
-                .address(session.address)
+                .address(message.address)
                 .text("Hello %s... Thanks for adding me. Just Tell me how can I help your dull and boring LIFE ../.. .", name || 'there');
         session.send(reply);
-        builder.Prompts.text(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+        
     },
     function (session, results) {
         session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
