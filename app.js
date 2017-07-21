564c1e674e809c7a4e88bf11030f9858d97b5922
Marrah <30308273+Kida021@users.noreply.github.com>
2017-07-21 16:20:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,56 +15,29 @@ var connector = new builder.ChatConnector({
     appPassword: "QcDi824W8C1oof6eaAu63J9"
 });
 // This is a dinner reservation bot that uses multiple dialogs to prompt users for input.
+// This is a reservation bot that has a menu of offerings.
 var bot = new builder.UniversalBot(connector, [
-    function (session) {
-        session.send("Welcome to the dinner reservation.");
-        session.beginDialog('askForDateTime');
-    },
-    function (session, results) {
-        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
-        session.beginDialog('askForPartySize');
-    },
-    function (session, results) {
-        session.dialogData.partySize = results.response;
-        session.beginDialog('askForReserverName');
-    },
-    function (session, results) {
-        session.dialogData.reservationName = results.response;
-
-        // Process request and display reservation details
-        session.send("Reservation confirmed. Reservation details: <br/>Date/Time: %s <br/>Party size: %s <br/>Reservation name: %s",
-            session.dialogData.reservationDate, session.dialogData.partySize, session.dialogData.reservationName);
-        session.endDialog();
+    function(session){
+        session.send("Welcome to Contoso Hotel and Resort.");
+        session.beginDialog("mainMenu");
     }
 ]);
 
-// Dialog to ask for a date and time
-bot.dialog('askForDateTime', [
-    function (session) {
-        builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+// Display the main menu and start a new request depending on user input.
+bot.dialog("mainMenu", [
+    function(session){
+        builder.Prompts.choice(session, "Main Menu:", menuItems);
     },
-    function (session, results) {
-        session.endDialogWithResult(results);
-    }
-]);
-
-// Dialog to ask for number of people in the party
-bot.dialog('askForPartySize', [
-    function (session) {
-        builder.Prompts.text(session, "How many people are in your party?");
-    },
-    function (session, results) {
-        session.endDialogWithResult(results);
+    function(session, results){
+        if(results.response){
+            session.beginDialog(menuItems[results.response.entity].item);
+        }
     }
 ])
-
-// Dialog to ask for the reservation name.
-bot.dialog('askForReserverName', [
-    function (session) {
-        builder.Prompts.text(session, "Who's name will this reservation be under?");
-    },
-    function (session, results) {
-        session.endDialogWithResult(results);
-    }
-]);
+.triggerAction({
+    // The user can request this at any time.
+    // Once triggered, it clears the stack and prompts the main menu again.
+    matches: /^main menu$/i,
+    confirmPrompt: "This will cancel your request. Are you sure?"
+});
 server.post('/api/messages', connector.listen());
