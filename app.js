b04f4a8a10a2d01ff238f8ce1c39ad0defed8fce
Marrah <30308273+Kida021@users.noreply.github.com>
2017-07-21 17:59:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -126,6 +126,12 @@ mapantayan nila ang lakas at galing ni GOD JIBANYA.... sila ay isang magikarp ln
          session.send({attachments: [{contenttype: 'image/jpg', contenturl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmX2BhGWtH5H0nR5q-_ZiHTgdWasVzqkNMgyOCw0QID16Ep4VtZg'}]});
          session.send(`(finger)`);
          session.send(`(finger)`);
+      }else if(session.message.text.toLowerCase().contains('shit') | session.message.text.toLowerCase().contains('(sheet)')
+               | session.message.text.toLowerCase().contains('sheep')){
+         session.send(`(poop)`);
+         session.send({attachments: [{contenttype: 'image/jpg', contenturl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmX2BhGWtH5H0nR5q-_ZiHTgdWasVzqkNMgyOCw0QID16Ep4VtZg'}]});
+         session.send(`(poop)`);
+         session.send(`(sheep)`);
       }else{
         session.send(`Sorry I don't understand alien language please learn how to speak in english!! and talk to me again.....\n
 But you can try these commands \n
