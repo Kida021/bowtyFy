cbab781a3a3dfd7aa13ef109efa856e6d6a559e5
Marrah <30308273+Kida021@users.noreply.github.com>
2017-07-21 08:46:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -108,6 +108,9 @@ mapantayan nila ang lakas at galing ni GOD JIBANYA.... sila ay isang magikarp ln
       }else if(session.message.text.toLowerCase().contains('rip') | session.message.text.toLowerCase().contains('r.i.p')){
          session.send(`YOU DIED ALONE!!`);
          session.send({attachments: [{contenttype: 'image/jpg', contenturl: 'https://i.imgflip.com/915lv.jpg?a416496'}]});
+      }else if(session.message.text.toLowerCase().contains('linkin park') | session.message.text.toLowerCase().contains('chester')){
+         session.send(`we've lost another legend ;(!!`);
+         session.send({attachments: [{contenttype: 'image/jpg', contenturl: 'https://media.makeameme.org/created/rip-in-the.jpg'}]});
       }else if(session.message.text.toLowerCase().contains('noob') | session.message.text.toLowerCase().contains('bobo')){
          session.send({attachments: [{contenttype: 'image/jpg', contenturl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmX2BhGWtH5H0nR5q-_ZiHTgdWasVzqkNMgyOCw0QID16Ep4VtZg'}]});
       }else{
